80f9fedab9338a51da198d791c97c9cf0355fd17
VirtualRedy <gustavo_alfonso@hotmail.it>
2025-05-11 22:20:45 +0200

    social contacts started

---
+++ components/SocialsSpan.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const myEmail = "vittorioandrea.sellaro@gmail.com";
+
+export default function SocialsSpan() {
+  const [emailSpan, setEmailSpan] = useState<string>("");
+  return (
+    <div className="flex flex-col justify-center items-center text-4xl font-bold mb-4 text-gray-700">
+      <h1>I miei riferimenti</h1>
+      <div className="flex gap-4 mt-4">
+        {/* Facebook */}
+        <Link
+          href="https://www.facebook.com/DesignDataUsers"
+          passHref
+          target="_blank"
+          className="h-8 w-8"
+        >
+          <Image
+            src="/logos/facebook-logo.png"
+            alt="facebook-logo"
+            width={32}
+            height={32}
+            className="object-cover"
+          />
+        </Link>
+        {/* Instagram */}
+        <Link
+          href="https://www.instagram.com/ddu_bimcommunity/"
+          passHref
+          target="_blank"
+          className="w-8 h-8"
+        >
+          <Image
+            src="/logos/instagram-logo.png"
+            alt="instagram-logo"
+            width={36}
+            height={36}
+            className="object-contain"
+          />
+        </Link>
+        {/* Linkedin */}
+        <Link
+          href="https://www.linkedin.com/in/vittorio-andrea-sellaro-9a5b87177/"
+          passHref
+          target="_blank"
+          className="w-8 h-8"
+        >
+          <Image
+            src="/logos/linkedin-logo.png"
+            alt="linkedin-logo"
+            width={36}
+            height={36}
+            className="object-contain"
+          />
+        </Link>
+        {/* mail */}
+        <div
+          className="w-8 h-8"
+          onClick={() => setEmailSpan(emailSpan ? "" : myEmail)}
+        >
+          <Image
+            src="/logos/email-logo.png"
+            alt="email-logo"
+            width={36}
+            height={36}
+            className="object-contain"
+          />
+        </div>
+      </div>
+      {emailSpan ? (
+        <div className="text-gray-500 text-2xl mx-4 mt-4 border-2 border-gray-500 rounded-2xl p-4">
+          {emailSpan}
+        </div>
+      ) : null}
+    </div>
+  );
+}
